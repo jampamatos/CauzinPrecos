137799d3c5a1b2fa77f6b4c19d548ce17e86f6ac
Jampa Matos <65971901+jampamatos@users.noreply.github.com>
2024-05-10 17:00:08 +0000

    feat: Set up and test the /calculate route
    Implemented the /calculate route in the Flask application to handle form submissions. This route checks for file upload and captures other form data such as markup, additional value, and federal tax inclusion:
    - Ensured that the route correctly handles the absence of a file or no file selected scenario by redirecting back to the index page.
    - Temporarily returns a string displaying all received data for verification purposes.
    - Configured the server to run with debug mode enabled for easier troubleshooting during development.
    
    This setup forms the basis for the next steps, which will involve actual processing of the XML file and calculating the final prices based on the received data.

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    # Check if file sent
+    if 'xmlFile' not in request.files:
+        return redirect(url_for('index')) # Redirect to index if no file sent
+
+    file = request.files['xmlFile']
+    if file.filename == '':
+        return redirect(url_for('index')) # Redirect to index if no file selected
+
+    # Fetch other form data
+    markup = request.form['markup']
+    additional = request.form['additional']
+    include_tax = 'federalTax' in request.form
+
+    # Logic for XML processing and price calculation
+    # Temporarily, we'll view received data
+    return f"Received file {file.filename}; markup: {markup}; additional {additional}; tax: {include_tax}."
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000, debug=True)
